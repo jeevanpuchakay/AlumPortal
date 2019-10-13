04900547507c1ab9c5c864ffe4b9eb40d96eced1
jeevan <jeevangkr37@gmail.com>
2019-10-13 23:18:22 +0530

    Addition of Homepage of alumni portal

---
+++ src/Components/Pages/Homepage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Kard from '../Kards/Kardfor2k19'
+import 'react-bootstrap'
+
+export default class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                
+                <table>
+                    <h1 className="text-black text-center">Yearbook Portal </h1>
+                    <p className="text-center">
+                        Founded in 1807, John Wiley & Sons, Inc. has been a valued source of knowledge and understanding
+                    </p>
+                    <p className="text-center">
+                    'Navbar' is defined but never used 
+                    </p>
+                    <p className="text-center">
+                    'NavItem' is defined but never used
+                    </p>
+                </table>
+                <Kard/>
+            </div>
+        )
+    }
+}
