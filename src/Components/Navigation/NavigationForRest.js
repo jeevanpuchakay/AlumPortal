2af5f12262a3435cb1bc117251b016480ec45d09
jeevan <jeevangkr37@gmail.com>
2019-10-14 21:06:04 +0530

    Addition of Nav bar of Rest of the portal

---
+++ src/Components/Navigation/NavigationForRest.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import { makeStyles} from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+//import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Data from '../Data/FacultyData.json'
+
+
+
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}))
+
+
+export default function NavigationForRest() {
+  const classes = useStyles();
+
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={1}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+      <Divider/>
+      <a href="/Homepage2k19" style={{paddingTop: '80px',textDecoration: 'none',}}>
+      <text style={{textDecorationStyle:'solid',fontSize:'30px'}}>
+      Yearbook 2K19
+      </text>
+      </a>
+      <text style={{fontSize:'20px'}}>
+          Contents
+      </text>
+       <List>
+          {
+            Data.map((text,index) =>(
+            <a href={text.B} style={{ textDecoration: 'none', }}>
+            <ListItem>
+           <ListItemText primary={text.A} styles={{color:'blue'}} />
+           </ListItem>
+           <Divider/>
+            </a>
+            ))
+          }
+       </List>
+      </Drawer>
+    </div>
+  );
+}
