55904c91c60aa0b0633548e92dd6639ae22774be
jeevan <jeevangkr37@gmail.com>
2019-10-15 02:01:03 +0530

    Addition of Faculty Pages

---
+++ src/Components/Pages/Faculty2.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import NavigationForRest from '../Navigation/NavigationForRest'
+
+
+const Background="https://image.shutterstock.com/image-photo/university-alumni-graduate-education-message-260nw-559783975.jpg"
+
+class Faculty2 extends Component {
+    render(){
+        return (
+            <div>
+                <NavigationForRest/>
+                <img src={Background} alt="phoo"  style={{marginTop:'0px',marginLeft:'240px',alignSelf: 'center',width:'82%',height:'700px'}}/>
+                <div style={{marginLeft: '500px',alignSelf: 'center',position:'absolute',}}>
+
+                    <h1>
+                        hello from Faculty2
+                    </h1>
+                </div>
+            </div>
+        )
+    
+}}
+
+export default Faculty2;
