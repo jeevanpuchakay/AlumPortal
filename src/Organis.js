6800102e98c3b5747cb8a9d815a76bbf37f62fb5
jeevan <jeevangkr37@gmail.com>
2019-10-15 02:06:58 +0530

    Additin of routes to Students and faculty

--- src/Organis.js
+++ src/Organis.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import {Route} from 'react-router-dom'
 import Homepage from './Components/Pages/Homepage'
 import Homepage2k19 from './Components/Pages/Homepage2k19'
+import Faculty1 from './Components/Pages/Faculty1'
+import Faculty2 from './Components/Pages/Faculty2'
+import Faculty3 from './Components/Pages/Faculty3'
+import CSE2k19 from './Components/Pages/Students.js/CSE2k19'
+import EE2k19 from './Components/Pages/Students.js/EE2k19'
+import MEC2k19 from './Components/Pages/Students.js/MEC2k19'
+//import FacultyData from './Components/Data/FacultyData.json'
 
 
 export default class Organis extends Component {
@@ -10,6 +17,13 @@ export default class Organis extends Component {
             <div>
                 <Route exact path="/" component={Homepage}/>
                 <Route path="/Homepage2k19" component={Homepage2k19}/>
+                <Route path="/Faculty1" component={Faculty1}/>
+                <Route path="/Faculty2" component={Faculty2}/>
+                <Route path="/Faculty3" component={Faculty3}/>
+                <Route path="/CSE2k19" component={CSE2k19}/>
+                <Route path="/EE2k19" component={EE2k19}/>
+                <Route path="/MEC2k19" component={MEC2k19}/>
+
             </div>
         )
     }
