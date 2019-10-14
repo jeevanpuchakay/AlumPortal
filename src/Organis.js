5951b75955228185a263e58a60377f7e8878f50c
jeevan <jeevangkr37@gmail.com>
2019-10-14 21:08:35 +0530

    Addition of route to Home page 2k19

--- src/Organis.js
+++ src/Organis.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {Route} from 'react-router-dom'
 import Homepage from './Components/Pages/Homepage'
+import Homepage2k19 from './Components/Pages/Homepage2k19'
 
 
 export default class Organis extends Component {
@@ -8,6 +9,7 @@ export default class Organis extends Component {
         return (
             <div>
                 <Route exact path="/" component={Homepage}/>
+                <Route path="/Homepage2k19" component={Homepage2k19}/>
             </div>
         )
     }
