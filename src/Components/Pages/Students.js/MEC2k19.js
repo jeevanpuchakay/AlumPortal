8f8cfe8645042c614d3674597c3dce81e512a10b
jeevan <jeevangkr37@gmail.com>
2019-10-15 02:02:07 +0530

    Addition of Students pages

---
+++ src/Components/Pages/Students.js/MEC2k19.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import NavigationForRest from '../../Navigation/NavigationForRest'
+import StudentKard from '../../Kards/StudentKard'
+import Me16People from '../../Data/Me16People.json'
+
+
+import {Grid,Cell} from 'styled-css-grid';
+
+
+
+const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 5
+        }}
+    />
+);
+
+export default class CSE2k19 extends Component {
+    
+    constructor(){
+        super() 
+          this.state = {
+            data: Me16People
+          }}
+
+
+    render() {
+        return (
+            <div>
+            <NavigationForRest />
+            <h1 style={{marginTop:'0px',marginLeft:'240px',alignSelf: 'center'}}>
+            Machanical Engineering 2019
+            </h1>  
+            <ColoredLine color="black" />
+            <div style={{marginTop:'0px',marginLeft:'240px',alignSelf: 'center',justifyContent: 'space-around',overflow: 'auto',}}>
+            <Grid flow="columns" columns={3} style={{rowGap:"30px"}}>
+                {
+                    this.state.data.map(function(people,index){
+                   return(
+                    <Cell>
+                        <StudentKard key={people.C} nameOnCard={people.B}/>
+                    </Cell>
+                   );
+                
+                })
+                }
+            </Grid>
+                </div>
+
+            </div>
+        )
+    }
+}
