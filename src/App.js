c40562ba65a22dfb56c2ae4da05fda0143a22025
jeevan <jeevangkr37@gmail.com>
2019-10-20 17:28:44 +0530

    Organisation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css"
-import HomeNavbar from './Components/Navigation/HomeNavbar'
+//import HomeNavbar from './Components/Navigation/HomeNavbar'
 
 import Organis from './Organis'
 
-//  >
+// <HomeNavbar/> >
     
 //import NavigationForRest from './Components/Navigation/NavigationForRest'
 
@@ -14,9 +14,6 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <div>
-      <HomeNavbar/>
-      </div>
         <Organis/>
       </div>
     )
