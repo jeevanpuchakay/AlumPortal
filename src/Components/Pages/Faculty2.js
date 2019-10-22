a222052c945b50ad0f2ce19b9356436ab0752779
jeevan <jeevangkr37@gmail.com>
2019-10-22 13:35:58 +0530

    Fixation of css in faculty pages

--- src/Components/Pages/Faculty2.js
+++ src/Components/Pages/Faculty2.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NavigationForRest from '../Navigation/NavigationForRest'
+import HomeNavbar from './../Navigation/HomeNavbar'
 
 
 const Background="https://image.shutterstock.com/image-photo/university-alumni-graduate-education-message-260nw-559783975.jpg"
@@ -8,7 +9,12 @@ class Faculty2 extends Component {
     render(){
         return (
             <div>
-                <NavigationForRest/>
+            <div  style={{paddingTop:'0px',zIndex:'3',position:'fixed',width:"100%"}}>
+            <HomeNavbar/>
+            </div>
+            <div  style={{zIndex: "1",position:'absolute'}}>
+            <NavigationForRest/>
+            </div>
                 <img src={Background} alt="phoo"  style={{marginTop:'0px',marginLeft:'240px',alignSelf: 'center',width:'82%',height:'700px'}}/>
                 <div style={{marginLeft: '500px',alignSelf: 'center',position:'absolute',}}>
 
