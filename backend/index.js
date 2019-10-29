0cf65b4323d2d808758c18a3e860ea7b4e2419e4
jeevan <jeevangkr37@gmail.com>
2019-10-30 00:51:42 +0530

    Backend setup

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express=require('express');
+
+const mongoose=require('mongoose')
+
+const cors=require('cors');
+
+require('dotenv').config();
+
+const app=express();
+
+const port=process.env.PORT||5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri=process.env.URI;
+
+mongoose.connect(uri,{
+    useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true
+})
+
+
+const connection=mongoose.connection;
+
+connection.once('open',()=>{
+    console.log("connected to mongodb")
+})
+
+
+app.listen(port,()=>{
+    console.log(`Listening to port ${port}`)
+})
