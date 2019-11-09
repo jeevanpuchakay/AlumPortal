7805b537a71b506defc7c74247d26cb0f45a0b60
jeevan <jeevangkr37@gmail.com>
2019-11-09 23:07:07 +0530

    logo Change

--- src/Components/Navigation/HomeNavbar.js
+++ src/Components/Navigation/HomeNavbar.js
@@ -6,12 +6,14 @@ import './../../App.css'
 //import {Grid} from 'styled-css-grid'
 
 
+const logo="https://i.imgur.com/x6rronw.jpg?3";
+
 export default class HomeNavbar extends Component {
     render() {
         return (
             <div style={{margin:'0px',width:'100%'}}>
             <Navbar className="navbar-color" style={{height:'5%'}}>
-          <img src="https://i.imgur.com/ReBk5qo.jpg?2" alt="alumcell"/>
+          <img src={logo} alt="alumcell"/>
           <Container style={{overflow:'visible'}}>
             <table>
           <text className="text-white" style={{margin:"0px",fontSize:'300%'}}>
