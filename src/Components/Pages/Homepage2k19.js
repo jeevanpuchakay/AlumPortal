9557a66c390ba6b570276f0cbb27201cf51fa753
jeevan <jeevangkr37@gmail.com>
2019-11-10 02:05:23 +0530

    Addition of Image in 2k19 Homepage

--- src/Components/Pages/Homepage2k19.js
+++ src/Components/Pages/Homepage2k19.js
@@ -1,35 +1,45 @@
 import React, { Component } from 'react'
 
 import NavigationForRest from '../Navigation/NavigationForRest'
-import Divider from '@material-ui/core/Divider';
+//import Divider from '@material-ui/core/Divider';
 import HomeNavbar from '../Navigation/HomeNavbar'
 
-const Background="https://image.shutterstock.com/image-photo/university-alumni-graduate-education-message-260nw-559783975.jpg";
+
+
+import { Container,Col,Row } from 'react-bootstrap'
+//const Background="https://image.shutterstock.com/image-photo/university-alumni-graduate-education-message-260nw-559783975.jpg";
+import Footer from './../Kards/Footer'
+
+const Picture="https://i.imgur.com/vhbpQqY.jpg?3"
+// from './../Data/_K6A9276.JPG'
+
+
+
 
 
 export default class Homepage2k19 extends Component {
     render() {
         return (
-            <div style={{marginTop:"0px",overflow:'visible',paddingTop:'0px'}}>
-            <div  style={{zIndex:'3',position:'fixed',width:"100%"}}>
+            <div>
+            <div  style={{margin:'0',zIndex:'5',position:'fixed',width:"100%"}}>
             <HomeNavbar/>
             </div>
             <div  style={{zIndex: "1",position:'absolute'}}>
             <NavigationForRest/>
             </div>
-            <div style={{marginTop:"0px"}}>
-            <img src={Background} alt="phoo"  style={{marginTop:'0px',marginLeft:'240px',alignSelf: 'center',width:'82%',height:'700px'}}/>
-            </div>
-            <div style={{marginLeft: '500px',alignSelf: 'center',position:'absolute',}}>
-                <h1>
-                    Yearbook 2k19
-                </h1>
-                <p>
-                    message message message
-                    <Divider/>
-                    qoute quote quote
-                </p>
-            </div>
+           <Container style={{zIndex:'3',position:'absolute',marginLeft:'18rem',marginTop:'10rem'}}>
+               <Col>
+               <Row>
+           <h3 style={{alignContent: 'center',justifyContent:'center',display:'flex'}}> YearBook 2K19 Batch</h3>
+           </Row><Row>
+           <img src={Picture} alt="all" />
+           </Row>
+           </Col>
+           </Container>
+          
+           <div style={{zIndex:'10',position:'absolute',marginTop:'80rem'}}>
+           <Footer />
+           </div>
             </div>
         )
     }
