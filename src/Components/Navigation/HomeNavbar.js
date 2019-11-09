7359f8b525e7901b84c0605f9a5c65fe81e2485f
jeevan <jeevangkr37@gmail.com>
2019-11-09 23:11:43 +0530

    logo Change and Nav bar resize

--- src/Components/Navigation/HomeNavbar.js
+++ src/Components/Navigation/HomeNavbar.js
@@ -12,12 +12,12 @@ export default class HomeNavbar extends Component {
     render() {
         return (
             <div style={{margin:'0px',width:'100%'}}>
-            <Navbar className="navbar-color" style={{height:'5%'}}>
+            <Navbar className="navbar-color" style={{height:'7rem'}}>
           <img src={logo} alt="alumcell"/>
-          <Container style={{overflow:'visible'}}>
+          <Container style={{overflow:'visible',marginLeft:'2rem'}}>
             <table>
           <text className="text-white" style={{margin:"0px",fontSize:'300%'}}>
-          Alumni portal
+          Yearbook portal
         </text>
         <br />
         <div style={{marginTop:'5%'}}>
